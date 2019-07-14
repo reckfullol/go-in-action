d5cd8c2c8816fbcdd7744af761c4bce97299903b
Reckfullol <626431528@qq.com>
2019-07-14 23:52:08 +0800

    Create chapter_07.

---
+++ src/chapter_07/dictionary.go
@@ -0,0 +1,58 @@
+package dictionary
+
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+		return nil
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	// The delete built-in function deletes the element with the specified key
+	// (m[key]) from the map. If m is nil or there is no such element, delete
+	// is a no-op.
+	// func delete(m map[Type]Type1, key Type)
+	delete(d, word)
+}

cfa1cf80793df84af74e1c2d2bd9ebcc9bb0a349
Reckfullol <626431528@qq.com>
2019-07-20 21:20:46 +0800

    Create chapter_12.

---
+++ src/chapter_12/reflection.go
@@ -0,0 +1,41 @@
+package chapter_12
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	//fn("I still can't believe South Korea beat Germany 2-0 put them last in their group")
+
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
